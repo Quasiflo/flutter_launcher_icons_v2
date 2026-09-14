884b43962d5b10a1f6ff9750e6bea06ff5d11aea
Luke Aguilar <dev@rdxip.com>
2026-09-14 16:09:49 +1000

    chore: regenerate mocks

--- test/macos/macos_icon_generator_test.mocks.dart
+++ test/macos/macos_icon_generator_test.mocks.dart
@@ -65,6 +65,13 @@ class MockConfig extends _i1.Mock implements _i3.Config {
       ) as bool);
 
   @override
+  bool get hasAndroidAdaptiveRoundConfig => (super.noSuchMethod(
+        Invocation.getter(#hasAndroidAdaptiveRoundConfig),
+        returnValue: false,
+        returnValueForMissingStub: false,
+      ) as bool);
+
+  @override
   bool get hasPlatformConfig => (super.noSuchMethod(
         Invocation.getter(#hasPlatformConfig),
         returnValue: false,
